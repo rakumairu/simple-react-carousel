53af9e20967c5f4864ef0c1e79e0b4bd344dd29f
Rakumairu <demaspiraa@gmail.com>
2021-01-04 16:30:15 +0700

    finished part 2

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -7,6 +7,8 @@ const Carousel = (props) => {
     const [currentIndex, setCurrentIndex] = useState(0)
     const [length, setLength] = useState(children.length)
 
+    const [touchPosition, setTouchPosition] = useState(null)
+
     // Set the length to match current children from props
     useEffect(() => {
         setLength(children.length)
@@ -24,6 +26,32 @@ const Carousel = (props) => {
         }
     }
 
+    const handleTouchStart = (e) => {
+        const touchDown = e.touches[0].clientX
+        setTouchPosition(touchDown)
+    }
+
+    const handleTouchMove = (e) => {
+        const touchDown = touchPosition
+
+        if(touchDown === null) {
+            return
+        }
+
+        const currentTouch = e.touches[0].clientX
+        const diff = touchDown - currentTouch
+
+        if (diff > 5) {
+            next()
+        }
+
+        if (diff < -5) {
+            prev()
+        }
+
+        setTouchPosition(null)
+    }
+
     return (
         <div className="carousel-container">
             <div className="carousel-wrapper">
@@ -34,7 +62,11 @@ const Carousel = (props) => {
                         &lt;
                     </button>
                 }
-                <div className="carousel-content-wrapper">
+                <div
+                    className="carousel-content-wrapper"
+                    onTouchStart={handleTouchStart}
+                    onTouchMove={handleTouchMove}
+                >
                     <div
                         className="carousel-content"
                         style={{ transform: `translateX(-${currentIndex * 100}%)` }}
